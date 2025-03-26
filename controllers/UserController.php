788aadade70c6b6e6ee122fe6e85a13a2ebd95c8
P-S.Cypre <pscypre@gmail.com>
2025-03-25 23:35:21 -0400

    feat(userController) : création de usercontroller

---
+++ controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+/**
+ * Développeur assigné(s) : Pierre
+ * Entité : Classe 'UserController' de la couche Controller
+ */
+
+use App\Models\User;
+use App\Services\AuthService;
+use App\Services\ValidationService;
+
+
+?>
